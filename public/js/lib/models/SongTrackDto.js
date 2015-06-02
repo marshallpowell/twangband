f076495672cfca64e0558b664665140ffd469ee0
bitbox <bitbox@cupebid.com>
2015-06-02 16:59:13 -0400

    song dto added

---
+++ public/js/lib/models/SongTrackDto.js
@@ -0,0 +1,25 @@
+/**
+ * Created by mpowell on 4/16/2015.
+ */
+
+/**
+ * DTO is used to represent the track within a SongDTO
+ * This captures the actual Track - TrackDto
+ * And captures the Track's attributes within the song
+ */
+
+function SongTrackDto(){
+
+    this.trackDto;
+    this.volume;
+    this.gain;
+    this.position;
+    this.loop;
+    this.description;
+    this.songId;
+    this.dateCreated;
+    this.lastUpdated;
+    this.updatedBy;
+    this.createdBy;
+    this.delete;
+}
