7834903714b7107a0356992fb877b5b9a93ad4a1
Marshall Powell <marshallpowell@gmail.com>
2015-10-31 02:31:00 +0000

    adding in missing files

---
+++ routes/services/listData.js
@@ -0,0 +1,17 @@
+var logger = require(APP_LIB + 'util/Logger').getLogger(__filename);
+var tagDao = require(APP_LIB + 'dao/TagDao');
+
+exports = module.exports = function(req, res) {
+
+    logger.debug("****************************************enter lists with type: " + req.query.type);
+    var listData = [];
+
+    var listType = req.query.type;
+
+    if (listType == 'instruments') {
+
+        listData = tagDao.getInstruments();
+        res.json(listData);
+        return;
+    }
+}
