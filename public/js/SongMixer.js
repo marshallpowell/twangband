c1972dec7ead408c0d9d715e941dc6f4214a6672
marshallpowell <marshallpowell@gmail.com>
2015-11-09 22:47:43 -0500

    test

---
+++ public/js/SongMixer.js
@@ -0,0 +1,56 @@
+
+
+
+
+
+logger.log('test2');
+
+
+
+
+
+var SongMixer = {};
+
+/**
+ *
+ * @param songDto
+ */
+SongMixer.init = function(songDto){
+
+};
+
+/**
+ *
+ */
+SongMixer.save = function(){
+
+};
+
+/**
+ *
+ */
+SongMixer.playPauseAll = function(){
+
+};
+
+/**
+ *
+ */
+SongMixer.addTrack = function(){
+
+};
+
+/**
+ *
+ */
+SongMixer.removeTrack = function(){
+
+};
+
+/**
+ *
+ * @param userDto
+ */
+SongMixer.addCollaborator = function(userDto){
+
+};
