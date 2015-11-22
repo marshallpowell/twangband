481207601595e06be4111c60ed75d9f9f2b8fd70
marshallpowell <marshallpowell@gmail.com>
2015-11-22 17:06:39 -0500

    changed server listen command to include IP

--- server.js
+++ server.js
@@ -136,4 +136,7 @@ global.logger = new (winston.Logger)({
 require('./routes')(app);
 
 app.listen(server_port);
-console.log('Running on http://localhost:' + server_port);
+
+app.listen( server_port, server_ip_address, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
